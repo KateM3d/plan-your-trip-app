673a976f8db16272da77efda32b3a9edff732ac0
KateM3d <ekaterina8medvedeva@gmail.com>
2026-06-26 14:03:14 +0100

    setup alembic

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,12 +1,20 @@
+import os
 from sqlalchemy.orm.session import Session
+from dotenv import load_dotenv
 
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./planyourtrip.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
---
+++ backend/app/models/trip_request.py
@@ -0,0 +1,24 @@
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, func
+from app.db.session import Base
+from datetime import datetime, timezone
+
+
+class TripRequest(Base):
+    __tablename__ = "trip_requests"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    destination = Column(String, nullable=False)
+    starting_location = Column(String, nullable=False)
+    budget = Column(Integer, nullable=False)
+    currency = Column(String, default="EUR")
+    travelers = Column(Integer, nullable=False)
+    departure_datetime = Column(DateTime(timezone=True), nullable=False)
+    return_datetime = Column(DateTime(timezone=True), nullable=False)
+    notes = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    is_active = Column(Boolean, default=True)
+    is_completed = Column(Boolean, default=False)
+    is_deleted = Column(Boolean, default=False)
